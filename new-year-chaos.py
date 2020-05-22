f0a8431ea36621ce5349c1f5ad1209c075239331
MaxwelSantana <maxwelsant96@icloud.com>
2020-05-21 22:16:39 -0300

    Update new-year-chaos.py

--- new-year-chaos.py
+++ new-year-chaos.py
@@ -16,13 +16,46 @@ def rotate_queue(index, value, initial_queue):
     return rotate_count
 
 
+def rotate_queue_v2(index, value, initial_queue):
+    if value not in initial_queue:
+        return -1
+    index_queue = initial_queue.index(value)
+
+    distance = (value - 1) - index
+    distance_invert_sinal = distance * -1
+    direction = 1 if distance_invert_sinal >= 0 else -1
+
+    rotate_count = 0
+    for i in range(index_queue, index_queue - abs(distance), direction):
+        if rotate_count == 2:
+            return -1
+        aux = initial_queue[i + direction]
+        initial_queue[i + direction] = initial_queue[i]
+        initial_queue[i] = aux
+        rotate_count += 1
+
+    return rotate_count
+
+
 # Complete the minimumBribes function below.
+def tiny_list(arr):
+    if len(arr) < 8:
+        arr = arr[0:]
+    else:
+        arr = arr[1:]
+    last = arr[len(arr) - 1]
+    arr.append(last + 1)
+    arr.append(last + 2)
+    return arr
+
+
 def minimumBribes(q):
     total_bribes = 0
-    initial_queue = list(range(1, len(q) + 1))
+    initial_queue = list(range(1, 3 + 1))
     is_too_chaotic = False
     for index, value in enumerate(q):
-        rotate_count = rotate_queue(index, value, initial_queue)
+        initial_queue = tiny_list(initial_queue)
+        rotate_count = rotate_queue_v2(index, value, initial_queue)
         if rotate_count < 0:
             is_too_chaotic = True
             break
@@ -34,4 +67,49 @@ def minimumBribes(q):
         print(total_bribes)
 
 
-print(minimumBribes([2, 1, 5, 3, 4]))
+def minimumBribes_v2(Q):
+    #
+    # initialize the number of moves
+    moves = 0
+    #
+    # decrease Q by 1 to make index-matching more intuitive
+    # so that our values go from 0 to N-1, just like our
+    # indices.  (Not necessary but makes it easier to
+    # understand.)
+    Q = [P - 1 for P in Q]
+    #
+    # Loop through each person (P) in the queue (Q)
+    for i, P in enumerate(Q):
+        # i is the current position of P, while P is the
+        # original position of P.
+        #
+        # First check if any P is more than two ahead of
+        # its original position
+        if P - i > 2:
+            print("Too chaotic")
+            return
+        #
+        # From here on out, we don't care if P has moved
+        # forwards, it is better to count how many times
+        # P has RECEIVED a bribe, by looking at who is
+        # ahead of P.  P's original position is the value
+        # of P.
+        # Anyone who bribed P cannot get to higher than
+        # one position in front if P's original position,
+        # so we need to look from one position in front
+        # of P's original position to one in front of P's
+        # current position, and see how many of those
+        # positions in Q contain a number large than P.
+        # In other words we will look from P-1 to i-1,
+        # which in Python is range(P-1,i-1+1), or simply
+        # range(P-1,i).  To make sure we don't try an
+        # index less than zero, replace P-1 with
+        # max(P-1,0)
+        for j in range(max(P - 1, 0), i):
+            if Q[j] > P:
+                moves += 1
+    print(moves)
+
+
+if __name__ == '__main__':
+    minimumBribes_v2([1, 2, 5, 3, 7, 8, 6, 4])
